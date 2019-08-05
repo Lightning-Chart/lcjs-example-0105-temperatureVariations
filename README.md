2f1c784d4cf3b85171c0d883b076412d42e985fe
Ilkka Kuosmanen <ilkka.kuosmanen@arction.com>
2019-08-05 13:40:13 +0300

    Initial

---
+++ README.md
@@ -0,0 +1,87 @@
+# Temperature Variations
+
+
+This demo application belongs to the set of examples for LightningChart JS, data visualization library for JavaScript.
+
+LightningChart JS is entirely GPU accelerated and performance optimized charting library for presenting massive amounts of data. It offers an easy way of creating sophisticated and interactive charts and adding them to your website or web application.
+
+The demo can be used as an example or a seed project. Local execution requires the following steps:
+
+- Make sure that relevant version of [Node.js](https://nodejs.org/en/download/) is installed
+- Open the project folder in a terminal:
+
+        npm install              # fetches dependencies
+        npm start                # builds an application and starts the development server
+
+- The application is available at *http://localhost:8080* in your browser, webpack-dev-server provides hot reload functionality.
+
+### Description
+
+The example shows the basic usage of Area Range series to display variation in temperature.
+
+Range charts are generally used to show variations (low & high) simultaneously in the given period.
+
+```javascript
+// Create a new ChartXY.
+const chart = lightningChart().ChartXY()
+
+// Add an area series with bipolar direction using default X and Y axes.
+const areaRange = chart.addAreaRangeSeries()
+```
+
+The series accepts ***AreaPoint*** type of points either as an object in format `{ position: number, high: number, low: number }`,
+
+```javascript
+series.add({ position: 20, high: 45, low: -20 })
+```
+
+or via a factory that should be additionally imported.
+
+```javascript
+series.add( AreaPoint( 20, 45, -20 ) )
+```
+
+Any number of points can be added with a single call.
+
+```javascript
+// Single point.
+series.add({ position: 20, high: 45, low: -20 })
+
+// Multiple points at once.
+series.add([
+    { position: 20, high: 45, low: -20 },
+    { position: 40, high: 95, low: 10 },
+    { position: 60, high: 25, low: 60 }
+])
+```
+
+### API links
+
+* XY cartesian chart: [ChartXY][]
+* Area point: [AreaPoint][]
+* Area Range series: [AreaRangeSeries][]
+
+
+### Support
+
+If you notice an error in the example code, please open an issue on [GitHub][0] repository of the entire example.
+
+Official [API documentation][1] can be found on [Arction][2] website.
+
+If the docs and other materials do not solve your problem as well as implementation help is needed, ask on [StackOverflow][3] (tagged lightningchart).
+
+If you think you found a bug in the LightningChart JavaScript library, please contact support@arction.com.
+
+Direct developer email support can be purchased through a [Support Plan][4] or by contacting sales@arction.com.
+
+© Arction Ltd 2009-2019. All rights reserved.
+
+[0]: https://github.com/Arction/
+[1]: https://www.arction.com/lightningchart-js-api-documentation/
+[2]: https://www.arction.com
+[3]: https://stackoverflow.com/questions/tagged/lightningchart
+[4]: https://www.arction.com/support-services/
+
+[AreaPoint]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/interfaces/areapoint.html
+[AreaRangeSeries]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/arearangeseries.html
+[ChartXY]: https://www.arction.com/lightningchart-js-api-documentation/v1.0.0/classes/chartxy.html
---
+++ src/index.js
@@ -0,0 +1,155 @@
+/**
+ * LightningChartJS example that showcases a simulation of daily temperature variations.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    AxisTickStrategies,
+    SolidFill,
+    SolidLine,
+    ColorRGBA,
+    LegendBoxBuilders,
+    UIOrigins,
+    emptyLine
+} = lcjs
+
+// Decide on an origin for DateTime axis.
+const dateOrigin = new Date(2019, 3, 1)
+
+// Create a XY Chart.
+const chart = lightningChart().ChartXY({
+    defaultAxisXTickStrategy: AxisTickStrategies.DateTime(dateOrigin)
+})
+    .setTitle('Daily temperature range, April 2019')
+
+const axisX = chart.getDefaultAxisX()
+const axisY = chart.getDefaultAxisY()
+    .setTitle('Temperature (°C)')
+    .setScrollStrategy(undefined)
+
+// Daily temperature records
+const recordRange = chart.addAreaRangeSeries()
+// Current month daily temperature variations
+const currentRange = chart.addAreaRangeSeries()
+// ----- Series stylings
+// Temperature records fill style
+const recordRangeFillStyle = new SolidFill({ color: ColorRGBA(100, 100, 100, 80) })
+// Record range stroke fill style, high line
+const recordRangeStrokeFillStyleHigh = new SolidLine().setFillStyle(new SolidFill({ color: ColorRGBA(250, 91, 70) }))
+// Record range stroke fill style, low line
+const recordRangeStrokeFillStyleLow = new SolidLine().setFillStyle(new SolidFill({ color: ColorRGBA(63, 138, 250) }))
+// Current month temperature fill style
+const currentRangeFillStyle = new SolidFill({ color: ColorRGBA(255, 174, 0, 200) })
+// Current range stroke fill style
+const currentRangeStrokeFillStyle = new SolidLine().setFillStyle(new SolidFill({ color: ColorRGBA(250, 226, 105) }))
+// ----- Applying stylings
+// Record range
+recordRange
+    .setName('Temperature records range')
+    .setHighFillStyle(recordRangeFillStyle)
+    // Same fill style for the highlighted series
+    .setHighFillStyleHighlight(recordRangeFillStyle)
+    .setHighStrokeStyle(recordRangeStrokeFillStyleHigh)
+    .setHighStrokeStyleHighlight(recordRangeStrokeFillStyleHigh)
+    .setLowStrokeStyle(recordRangeStrokeFillStyleLow)
+    .setLowStrokeStyleHighlight(recordRangeStrokeFillStyleLow)
+// Current range
+currentRange
+    .setName('2019 temperatures')
+    .setHighFillStyle(currentRangeFillStyle)
+    .setHighStrokeStyle(currentRangeStrokeFillStyle)
+    .setLowStrokeStyle(currentRangeStrokeFillStyle)
+
+// ----- Result tables settings
+// Record range
+recordRange.setResultTableFormatter((builder, series, figure, yMax, yMin) => {
+    return builder
+        .addRow('Temperature records range')
+        .addRow('Date: ' + axisX.formatValue(figure))
+        .addRow('Highest: ' + yMax.toFixed(2) + ' °C')
+        .addRow('Lowest: ' + yMin.toFixed(2) + ' °C')
+})
+// Current range
+currentRange.setResultTableFormatter((builder, series, figure, yMax, yMin) => {
+    return builder
+        .addRow('2019 temperatures')
+        .addRow('Date: ' + axisX.formatValue(figure))
+        .addRow('Highest: ' + yMax.toFixed(2) + ' °C')
+        .addRow('Lowest: ' + yMin.toFixed(2) + ' °C')
+})
+// ----- Generating data
+const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+}
+const currentRangeData = []
+const recordRangeData = []
+// Current range
+for (let i = 0; i < 31; i++) {
+    const randomPoint = () => {
+        const x = i
+        let yMax
+        if (i > 0) {
+            const previousYMax = currentRangeData[i - 1].yMax
+            yMax = randomInt(previousYMax - 5, previousYMax + 5)
+        } else {
+            yMax = randomInt(-5, 25)
+        }
+        const yMin = randomInt(yMax - 5, yMax) - 5
+        return {
+            x,
+            yMax,
+            yMin
+        }
+    }
+    currentRangeData.push(randomPoint())
+}
+
+let recordYMax = currentRangeData[0].yMax
+let recordYMin = currentRangeData[0].yMin
+for (let i = 1; i < currentRangeData.length; i++) {
+    if (currentRangeData[i].yMin < recordYMin) recordYMin = currentRangeData[i].yMin
+    if (currentRangeData[i].yMax > recordYMax) recordYMax = currentRangeData[i].yMax
+}
+// Set series interval
+axisY.setInterval(recordYMin - 5, recordYMax + 5)
+// ----- Generate record temperatures
+for (let i = 0; i < 31; i++) {
+    const randomPoint = () => {
+        const x = i
+        const yMax = randomInt(recordYMax - 2, recordYMax + 2)
+        const yMin = randomInt(recordYMin - 1, recordYMin)
+        return {
+            x,
+            yMax,
+            yMin
+        }
+    }
+    recordRangeData.push(randomPoint())
+}
+// ----- Adding data points
+recordRangeData.forEach((point, i) => {
+    recordRange.add({ position: (point.x * 24 * 60 * 60 * 1000), high: point.yMax, low: point.yMin })
+})
+
+currentRangeData.forEach((point, i) => {
+    currentRange.add({ position: (point.x * 24 * 60 * 60 * 1000), high: point.yMax, low: point.yMin })
+})
+// ----- Add legend box
+const legendBox = chart.addLegendBox(LegendBoxBuilders.HorizontalLegendBox, {
+    x: axisX.scale,
+    y: axisY.scale
+})
+    .setPosition({ x: currentRange.getXMax() / 2, y: axisY.scale.getInnerStart() })
+    .setOrigin(UIOrigins.CenterBottom)
+    .setMargin(3)
+
+const entries = legendBox.add(chart)
+entries[0]
+    .setButtonOnFillStyle(recordRangeFillStyle)
+    .setButtonOnStrokeStyle(emptyLine)
+entries[1]
+    .setButtonOnFillStyle(currentRangeFillStyle)
+    .setButtonOnStrokeStyle(emptyLine)
